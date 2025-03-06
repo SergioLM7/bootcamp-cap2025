38e80ca4dadf8dd4dda9583a8b4011a12645096a
Sergio Lillo <sergiolillom@gmail.com>
2025-03-06 15:20:40 +0100

    "Christmas tree in main and Hibernate + Lombok"

--- demo-maven/src/main/java/com/example/demo_maven/App.java
+++ demo-maven/src/main/java/com/example/demo_maven/App.java
@@ -6,8 +6,15 @@ package com.example.demo_maven;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String branch = "*";
+        for(int i =0; i < 6; i++) {
+            System.out.println(branch);
+        	branch += "*";
+        }
+     
     }
 }
