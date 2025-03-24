0bb38fe4aaf511bc3b867776e52446a52b02bce0
Sergio Lillo <sergiolillom@gmail.com>
2025-03-24 15:49:16 +0100

    "First laboratory for SOAP Web Service Producer"

---
+++ laboratorio-soap-web/laboratorio-soap-web/src/test/java/com/example/laboratorio_soap_web/LaboratorioSoapWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio_soap_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioSoapWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
