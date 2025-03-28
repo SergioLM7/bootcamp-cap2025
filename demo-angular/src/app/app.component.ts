d6106cc9722855bc2a34503f830f4126affe4828
Sergio Lillo <sergiolillom@gmail.com>
2025-03-28 13:48:53 +0100

    Laboratory notifications system Done

--- demo-angular/src/app/app.component.ts
+++ demo-angular/src/app/app.component.ts
@@ -1,20 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoggerService } from '@my/core';
+// import { LoggerService } from '@my/core';
+import { DemosComponent } from './demos/demos.component';
+import { NotificationModalComponent } from './main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotificationModalComponent, CommonModule, DemosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title: string = 'Sergio';
-  constructor(out: LoggerService) {
-    out.error("Es un error");
-    out.warn("Es un warn");
-    out.info("Es un info");
-    out.log("Es un log");
-  }
+  // constructor(out: LoggerService) {
+  //   out.error("Es un error");
+  //   out.warn("Es un warn");
+  //   out.info("Es un info");
+  //   out.log("Es un log");
+  // }
 }
  
