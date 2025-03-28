50137bbbfc89de7048e463b2ee5ce8dda3addc26
Sergio Lillo <sergiolillom@gmail.com>
2025-03-28 09:40:51 +0100

    folders structure Done

---
+++ demo-angular/src/app/security/security.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyCoreModule } from '../../lib/my-core';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, MyCoreModule
+  ]
+})
+export class SecurityModule { }
--- demo-angular/src/environments/environment.ts
+++ demo-angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000',
-    securityUrl: 'http://localhost:3000',
+    apiUrl: '/api/catalogo/',
+    securityUrl: '/auth',
 };
