3464438fcc693e3006bffdf22484d4a6fb00f00d
Sergio Lillo <sergiolillom@gmail.com>
2025-03-31 11:33:34 +0200

    Home component created

--- demo-angular/src/app/app.component.ts
+++ demo-angular/src/app/app.component.ts
@@ -3,16 +3,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { LoggerService } from '@my/core';
 import { DemosComponent } from './demos/demos.component';
-import { NotificationModalComponent } from './main';
+import { HomeComponent, NotificationModalComponent } from './main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NotificationModalComponent, CommonModule, DemosComponent],
+  imports: [RouterOutlet, NotificationModalComponent, CommonModule, DemosComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'Sergio';
   // constructor(out: LoggerService) {
   //   out.error("Es un error");
   //   out.warn("Es un warn");
