fe76aca833f1cf56a686134b972ee1c8ced29e3d
Sergio Lillo <sergiolillom@gmail.com>
2025-04-01 11:26:04 +0200

    Formularios component created

--- demo-angular/src/app/app.component.ts
+++ demo-angular/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { LoggerService } from '@my/core';
-import { DemosComponent } from './demos/demos.component';
+//import { DemosComponent } from './demos/demos.component';
 import { HomeComponent, NotificationModalComponent } from './main';
+import { FormulariosComponent } from './ejemplos/formularios/formularios.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NotificationModalComponent, CommonModule, DemosComponent, HomeComponent],
+  imports: [RouterOutlet, NotificationModalComponent, CommonModule, FormulariosComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
